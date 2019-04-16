5e2bb6802c64913353106a8755af7185d2e74e99
Brad Spencer <bradspencer@Brads-Mac-mini.local>
2019-04-16 14:12:51 -0400

    font fix

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,8 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Work+Sans');
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Work Sans', sans-serif;
 }
 </style>
