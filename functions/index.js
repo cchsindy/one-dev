2309f2b2df367e3b64975ee488757b0e72836c97
Brad Spencer <bradspencer@Brads-Mac-mini.local>
2019-04-18 16:27:32 -0400

    added SkyService and got function working

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,9 @@
 const functions = require('firebase-functions');
+const SkyService = require('./services/blackbaud/SkyService')
+
+exports.blackbaud = functions.https.onRequest((request, response) => {
+  response.send("run a BB thing...")
+})
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
