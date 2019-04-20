6c3b886ed198dbf62da1fed4aae58d8f2a3b034c
Brad Spencer <bradspencer@covenantchristian.org>
2019-04-20 09:37:19 -0400

    not much

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import * as firebase from '@/store/modules/firebase'
 import announcements from '@/store/modules/announcements'
+import blackbaud from '@/store/modules/blackbaud'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    announcements
+    announcements,
+    blackbaud
   },
   state: {
     fbStore: firebase.myStore
--- src/views/Blackbaud.vue
+++ src/views/Blackbaud.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Blackbaud</h1>
-    <BaseButton @click="callBB">Create Doc</BaseButton>
+    <BaseButton @click="callBB">Get Constituent</BaseButton>
   </div>
 </template>
 
@@ -9,11 +9,7 @@
   export default {
     methods: {
       callBB() {
-        const col = this.$store.state.fbStore.collection('skyRequests')
-        col.add({
-          topic: 'programming',
-          language: 'javascript'
-        })
+        
       }
     }
   }
