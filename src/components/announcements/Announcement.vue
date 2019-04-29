934d269cfa83b0a4430539c034e7d921ac5ba2d1
Brad Spencer <bradspencer@Brads-Mac-mini.local>
2019-04-29 15:59:15 -0400

    added validation

--- src/components/announcements/Announcement.vue
+++ src/components/announcements/Announcement.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="announcement-item">
     <BaseButton @click="removeAnnouncement" :disabled="isNew" class="small">Remove</BaseButton>
-    <BaseButton @click="saveAnnouncement" :disabled="hideSave" class="small">Save</BaseButton>
-    <BaseButton @click="cancelAnnouncement" :disabled="hideSave" class="small">Cancel</BaseButton>
+    <BaseButton @click="saveAnnouncement" :disabled="hideSave || !isValid" class="small">Save</BaseButton>
+    <BaseButton @click="cancelAnnouncement" :disabled="hideSave && !isNew" class="small">Cancel</BaseButton>
     <BaseContent label="Message:" v-model="announcement.message"/>
     <div class="announcement-date">
       <BaseInput label="From:" type="date" v-model="announcement.fromDate"/>
@@ -16,6 +16,12 @@ export default {
   computed: {
     isNew() {
       return this.announcement.id.substring(0, 3) === 'NEW'
+    },
+    isValid() {
+      let valid = true
+      if (this.announcement.message === '') valid = false
+      if (this.announcement.toDate < this.announcement.fromDate) valid = false
+      return valid
     }
   },
   data: () => {
