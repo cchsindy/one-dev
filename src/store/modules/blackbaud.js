de94c65dff7b108ad28afee1270a531f3dc8ac2a
bradocchs <bradspencer@covenantchristian.org>
2019-08-25 17:32:21 -0400

    changed to store

--- src/store/modules/blackbaud.js
+++ src/store/modules/blackbaud.js
@@ -1,5 +1,5 @@
 const state = {
-
+  blackbaudStudents: []
 }
 
 const getters = {
@@ -7,11 +7,26 @@ const getters = {
 }
 
 const actions = {
-
+  getBlackbaudStudents({ commit, rootState }) {
+    commit('CLEAR_BB_STUDENTS')
+    const sky = rootState.fbFunctions.httpsCallable('skyapi')
+    sky({ product: 'school', url: 'users/extended', params: {
+      base_role_ids: '14'
+      }
+    })
+    .then(result => {
+      commit('ADD_BB_STUDENTS', result.data.value)
+    })
+  }
 }
 
 const mutations = {
-  
+  ADD_BB_STUDENTS(state, data) {
+    state.blackbaudStudents = data
+  },
+  CLEAR_BB_STUDENTS(state) {
+    state.blackbaudStudents = []
+  }
 }
 
 export default {
