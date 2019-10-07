8d91ff74ce65a4b8353f9d15468a02749dabb05c
Brad Spencer <bradspencer@Brads-Mac-mini.local>
2019-10-07 14:53:41 -0400

    started course compare code

--- src/components/sync/StudentItem.vue
+++ src/components/sync/StudentItem.vue
@@ -3,11 +3,13 @@
     <b>{{item.last_name}},
     {{item.first_name}}:</b>
     <span class="bb" v-for="bb in item.bb_courses" :key="bb.id">
-      {{bb.code}}
+      <span v-if="bb.dropped === 0">
+      {{bb.section}}-{{bb.code}}
+      </span>
     </span>
     ::
     <span class="canvas" v-for="c in item.canvas_courses" :key="c.id">
-      {{c.code}}
+      {{c.section}}-{{c.code}}
     </span>
   </div>
 </template>
@@ -18,6 +20,32 @@
       item: {
         type: Object,
         required: true
+      }
+    },
+    watch: {
+      item: function () {
+        if (this.item.bb_courses && this.item.canvas_courses) {
+          for (let i = 0, leni = this.item.bb_courses.length; i < leni; i++) {
+            for (let j = 0, lenj = this.item.canvas_courses.length; j < lenj; j++) {
+              if (this.item.bb_courses[i].dropped === 0) { 
+                if (this.item.bb_courses[i].code === this.item.canvas_courses[j].code
+                    && this.item.bb_courses[i].section === this.item.canvas_courses[j].section) {
+                  this.item.bb_courses[i].code += '*'
+                  this.item.canvas_courses[j].code += '*'
+                  // this.item.bb_courses.splice(i, 1)
+                  // this.item.canvas_courses.splice(j, 1)
+                }
+              } else {
+                if (this.item.bb_courses[i].code === this.item.canvas_courses[j].code
+                    && this.item.bb_courses[i].section === this.item.canvas_courses[j].section) {
+                  this.item.canvas_courses[j].code += 'D'
+                  // this.item.bb_courses.splice(i, 1)
+                  // this.item.canvas_courses.splice(j, 1)
+                }
+              }
+            }
+          }
+        }
       }
     }
   }
